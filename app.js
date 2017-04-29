8f0467d9d7376c11cccd8ada2a6cd464f1590dbf
Nhan Ho <dorekido_1938@yahoo.com.vn>
2017-04-29 16:10:26 +0700

    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var app = angular.module('myApp', ['ngRoute']);
-app.config(function($routeProvider) {
+var myApp = angular.module('myApp', ['ngRoute']);
+myApp.config(function($routeProvider) {
     $routeProvider
         .when('/', {
             templateUrl: 'pages/home.html',
@@ -8,6 +8,10 @@ app.config(function($routeProvider) {
         .when('/admin', {
             templateUrl: 'pages/admin.html',
             controller: 'bookCtrl'
+        })
+        .when('/books/detail/:id', {
+            controller: 'bookCtrl',
+            templateUrl: 'pages/detail.html'
         })
         .otherwise({
             templateUrl: 'pages/home.html'
--- controllers/books.js
+++ controllers/books.js
@@ -1,6 +1,46 @@
-app.controller('bookCtrl', function($scope, $http) {
-    $http.get("https://green-web-bookstore.herokuapp.com/api/books")
-        .then(function(response) {
-            $scope.books = response.data;
+var myApp = angular.module('myApp');
+
+myApp.controller('bookCtrl', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams) {
+    console.log('bookCtrl loaded...');
+    var root = 'https://green-web-bookstore.herokuapp.com';
+    $scope.getBooks = function() {
+        $http.get(root + '/api/books').success(function(response) {
+            $scope.books = response;
         });
-});
+    }
+    $scope.getGenres = function() {
+        $http.get(root + '/api/genres').success(function(response) {
+            $scope.genres = response;
+        });
+    }
+
+    $scope.getBook = function() {
+        var id = $routeParams.id;
+        $http.get(root + '/api/books/' + id).success(function(response) {
+            $scope.book = response;
+        });
+    }
+
+    $scope.addBook = function() {
+        console.log($scope.book);
+
+
+
+        $http.post(root + '/api/books/', $scope.book).success(function(response) {
+            window.location.href = '#/books';
+        });
+    }
+
+    $scope.updateBook = function() {
+        var id = $routeParams.id;
+        $http.put(root + '/api/books/' + id, $scope.book).success(function(response) {
+            window.location.href = '#/books/' + $routeParams.id;
+        });
+    }
+
+    $scope.removeBook = function(id) {
+        $http.delete(root + '/api/books/' + id).success(function(response) {
+            window.location.href = '#/books';
+        });
+    }
+}]);
